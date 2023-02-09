7d0ccfc12c87cce337e8ecc0a0f9777581cdf2d5
NicholasPanait <nicholas.panait@ontariotechu.net>
2023-02-09 14:47:25 -0500

    setup for program

---
+++ src/frontend.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    
+    cout << "Welcome to Hubble Auction House";
+    while(true){
+        string command;
+        cout << "Please enter a command:";
+        cin >> command;
+        processCommand(command);
+    }
+}
+
+void processCommand(string command){
+    if (command == "login"){
+
+    }
+    else if (command == "create"){
+        
+    }
+    else if (command == "logout"){
+        
+    }
+    else if (command == "delete"){
+        
+    }
+    else if (command == "advertise"){
+        
+    }
+    else if (command == "bid"){
+        
+    }
+    else if (command == "refund"){
+        
+    }
+    else if (command == "addcredit"){
+        
+    }
+    else if (command == "listall"){
+        
+    }
+    else{
+        cout << "Invalid Command!";
+    }
+}
