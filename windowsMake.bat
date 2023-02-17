616f5547413524b9de8fa4d28b21d23c00ffbc04
Alexander Naylor <alexandernaylor@outlook.com>
2023-02-17 13:18:06 -0500

    modified readme and removed redundant windowsmake.bat

--- windowsMake.bat
+++
@@ -1,7 +0,0 @@
-wsl make
-
-::If this fails to run try these commands in order
-:: wsl sudo apt update
-:: wsl sudo apt upgrade
-:: wsl sudo apt install make
-:: wsl sudo apt install build-essential
