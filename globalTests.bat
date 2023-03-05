966f60d3d30c0a469c069ba7ed31d773f4b2bcc8
Alden Chan <aldenchan1213@gmail.com>
2023-03-05 00:58:40 -0500

    Added missing tests for no refund and non numeric refund

--- globalTests.bat
+++ globalTests.bat
@@ -1,3 +1,5 @@
 @echo off
 
 forfiles /p tests\ /s /m *.bat /c "cmd /c @path"
+PAUSE
+EXIT
---
+++ tests/frontend/refund/refund10/script.bat
@@ -0,0 +1,18 @@
+@echo off
+copy ..\..\globalTestItems.txt .\localTestItems.txt
+copy ..\..\globalTestUsers.txt .\localTestUsers.txt
+copy .\..\..\..\..\build\frontend.exe .\frontend.exe
+frontend.exe ./localTestUsers.txt ./localTestItems.txt ./ < ./inputStream.txt > ./output.txt
+
+fc output.txt expectedOutputStream.txt
+if errorlevel 1 goto different
+fc transactionFile.txt expectedTransactionFile.txt
+if errorlevel 1 goto different
+
+echo %cd% [92mTEST PASSED[0m
+EXIT
+
+:different
+echo %cd% [91mTEST FAILED[0m
+PAUSE
+EXIT
