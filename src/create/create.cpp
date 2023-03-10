9adaa55590beca06a379538c28316a18ffb061ac
Ryan De Sousa <ryan.desousa@ontariotechu.net>
2023-03-10 09:51:42 -0800

    made create only accept passwords with less characters than 15

--- src/create/create.cpp
+++ src/create/create.cpp
@@ -45,6 +45,13 @@ void Create()
         // TODO CHECK THE LENGTH OF THE PASSWORD
         cout << "Please enter the password for the account (must be less than 15 characters long):" << endl;
         getline(cin, password);
+        if (password.length() > 15){
+            cout << "password must be less than 16 characters long" << endl;
+            return;
+        } else if (password.length() < 1){
+            cout <<  "password must be at least 1 character long" << endl;
+            return;
+        }
 
         for (int i = 0; i < password.size(); i++){
             password_encrypted += int(password[i]);
