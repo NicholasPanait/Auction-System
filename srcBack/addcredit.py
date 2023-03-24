3ac772aa2b52e89683c8c3d98196aa017bb79677
Ryan De Sousa <ryan.desousa@ontariotechu.net>
2023-03-24 10:24:47 -0700

    implemented the add credit function

--- srcBack/addcredit.py
+++ srcBack/addcredit.py
@@ -1,4 +1,15 @@
+import utility
 #TODO: Increase the credit of an existing user and update the users list
 
 def addcredit(username, credit, users):
-    return
+    for i in users:
+        if (i.username == username):
+            float_cred = float(i.credit)
+            float_cred += float(credit)
+            str_cred = str(float_cred)
+            if (str_cred[-3] != "."):
+                str_cred+="0"
+
+            while(len(str_cred) < 9):
+                str_cred = "0"+str_cred
+            i.credit = str_cred
